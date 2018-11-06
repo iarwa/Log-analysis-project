10e8a813abc4ae65e9b0ff68c4d5a40cdcda1868
arwaabdullah <i.arwabdullah@gmail.com>
2018-11-06 23:10:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Logs Analysis Project
+
+This project aims to build an internal reporting tool that will use information from the database and use SQL queries to analyze the log data, and print out the answers to some questions about the site's user activity.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine.
+
+## Technologies used
+
+1. PostgreSQL
+2. Writing Python code with DB-API
+3. Linux-based virtual machine (VM) Vagrant
+
+### Prerequisites
+
+1. Download [Vagrant](https://www.vagrantup.com/) and install.
+2. Download [Virtual Box](https://www.virtualbox.org/) and install.
+3. Clone this repository to a directory of your choice.
+4. Download the [database file] (https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip) you will need to unzip this file after downloading it. The file inside is called **newsdata.sql**. Put this file and **newsDB.py** file from the repository into the **vagrant** directory within your VM.
+
+### Installing
+
+Run these commands from the terminal in the folder where your vagrant is installed in:
+
+1. ```vagrant up``` to start up the VM.
+2. ```vagrant ssh``` to log into the VM.
+3. ```cd /vagrant``` to change to your vagrant directory.
+4. ```psql -d news -f newsdata.sql``` to load the data and create the tables.
+5. ```python newsDB.py``` to run the reporting tool.
+
+## Versioning
+
+This project use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/iarwa/logs-analysis-project/tags).
+
+## Authors
+
+* **Arwa Alshathri** - *Initial work* - [iarwa](https://github.com/iarwa)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ newsDB.py
@@ -0,0 +1,75 @@
+
+
+import psycopg2
+
+DBNAME = "news"
+query1 = """ SELECT '"' || articles.title || '"' , COUNT(log.id) AS views FROM articles CROSS JOIN log WHERE log.path = '/article/' || articles.slug AND log.status = '200 OK' GROUP BY articles.id ORDER BY views DESC LIMIT 3; """
+query2 = """ SELECT authors.name, COUNT(log.id) AS views FROM authors JOIN articles ON authors.id = articles.author CROSS JOIN log WHERE log.path = '/article/' || articles.slug AND log.status = '200 OK' GROUP BY authors.name ORDER BY views DESC; """
+query3 = """ SELECT q1.day, ROUND(q2.views * 100.0 / q1.views, 2) AS error
+             FROM (SELECT TRIM(trailing ' ' FROM TO_CHAR(DATE(time), 'Month')) || ' ' || TO_CHAR(DATE(time), 'DD') || ', ' || TO_CHAR(DATE(time), 'YYYY') AS day, COUNT(id) AS views
+                   FROM log
+                   GROUP BY day)
+             AS q1
+
+             JOIN (SELECT TRIM(trailing ' ' FROM TO_CHAR(DATE(time), 'Month')) || ' ' || TO_CHAR(DATE(time), 'DD') || ', ' || TO_CHAR(DATE(time), 'YYYY') AS day, COUNT(id) AS views
+                   FROM log
+                   WHERE status = '404 NOT FOUND'
+                   GROUP BY day)
+             AS q2 ON q1.day = q2.day
+             WHERE ROUND(q2.views * 100.0 / q1.views, 2) > 1.00
+             ORDER BY error DESC
+             LIMIT 1; """
+
+def get_popular_articles():
+
+    """Return the most popular three articles of all time, most popular article at the top."""
+    try:
+        db = psycopg2.connect(dbname=DBNAME)
+        c = db.cursor()
+        c.execute(query1)
+        result = c.fetchall()
+        for article in result:
+            print("{} -- {} views".format(article[0], article[1]))
+        db.close()
+    except:
+        return "Unable to connect to the database"
+
+
+def get_popular_authors():
+
+    """Return the most popular article authors of all time, most popular author at the top."""
+    try:
+        db = psycopg2.connect(dbname=DBNAME)
+        c = db.cursor()
+        c.execute(query2)
+        result = c.fetchall()
+        for author in result:
+            print("{} -- {} views".format(author[0], author[1]))
+        db.close()
+    except:
+        return "Unable to connect to the database"
+
+
+def get_error_requests():
+
+    """Return which days with more than 1'%' of its requests lead to errors."""
+    try:
+        db = psycopg2.connect(dbname=DBNAME)
+        c = db.cursor()
+        c.execute(query3)
+        result = c.fetchall()
+        for errors in result:
+            print("{} -- {} % errors".format(errors[0], errors[1]))
+        db.close()
+    except:
+        return "Unable to connect to the database"
+
+if __name__ == "__main__":
+  print("\n**** Report Results ****\n\n(1)   The most popular three articles\n")
+  get_popular_articles()
+  print("\n-----------------------\n\n")
+  print("(2)   The most popular article authors\n")
+  get_popular_authors()
+  print("\n-----------------------\n\n")
+  print("(3)   Days with more than 1% of its requests lead to errors\n")
+  get_error_requests()
